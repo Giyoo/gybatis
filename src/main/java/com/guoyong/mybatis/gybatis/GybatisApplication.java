15ca5b6fd52469b5ab2b43b657705da944593ff2
郭勇 <guoyong@keking.cn>
2020-08-11 19:17:22 +0800

    初始化

---
+++ src/main/java/com/guoyong/mybatis/gybatis/GybatisApplication.java
@@ -0,0 +1,13 @@
+package com.guoyong.mybatis.gybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GybatisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/guoyong/mybatis/gybatis/GybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guoyong.mybatis.gybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
